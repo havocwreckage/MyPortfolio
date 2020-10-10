279eec0564518919f8376c2423edea5c0e638d45
havocwreckage <havoc.wreckage@gmail.com>
2020-10-10 16:24:02 -0400

    ok

---
+++ src/resumeData.js
@@ -0,0 +1,101 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Josue Flores",
+    "role": "Frontend Developer",
+    "linkedinId":"Havocwreckage",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I am a soon to be graduate from the Road to Hire program. The Road to Hire program is a 6-month program that aims to develop successful students in the software development field and Teamworking Exercise. Within the program, we have learned many skills such as working with Github, shell terminal, HTML and CSS, JavaScript, Node.js, Public Speaking, Team Building, and React at the moment. I believe that my progress through the program has change my views on life. Also working remotely has been a brand new experience I have come to enjoy. I know that Road to Hire has equipped me with the skills that will help me succeed. Also if you think You can beat me in Rainbow Six let us see. ",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/j-d-flores/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/havocwreckage",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"facebook",
+          "url":"https://www.facebook.com/profile.php?id=100022534762611",
+          "className":"fa fa-facebook"
+        }
+      ],
+    "aboutme":"I always have had the drive to learn more and do better and no matter what hold me back i rose to victory",
+    "address":"9516 stoney Glen dr Apt O charlotte nc 28216",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"Hopewell HighSchool",
+        "specialization":"Graduated",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2017",
+        "Achievements":"Diploma"
+      },
+      {
+        "UniversityName":"Road to HIRE Fellowship",
+        "specialization":"Learning HTML5, CSS ,REACTJS, NODEJS, MYSQL,",
+        "MonthOfPassing":"Feb 2020 -",
+        "YearOfPassing":"July 2020",
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Machine Operator",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Oct 2018 - ",
+        "YearOfLeaving":"Feb2020",
+        "Achievements":"Operate multiple laser printers simultaneously without a supervisor. Adhere to quality control procedures. Read and correctly complete job sheets. Identify production-related problems and notify the supervisor in a timely manner. Maintain logs of files received and released. Perform recovery processes when necessary within client SLA requirements. Perform minor repairs on Laser equipment Assist in other areas when needed."
+      },
+      {
+        "CompanyName":"Road to hire",
+        "specialization":"Fellow",
+        "MonthOfLeaving":"Jun",
+        "YearOfLeaving":"2020",
+        "Achievements":"Learned and Designed Apps using HTML, CSS, Node, React, and AWS. Professional Development for Public speaking skills and Interviews."
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"My First Project Ever",
+        "description":"A highschool Project made using Ruby on Rails",
+            "imgurl": "images/portfolio/phone.png",
+            "url": "https://talentmeet.herokuapp.com"
+      },
+      {
+        "name":"My first HTML5 ",
+        "description":"This was my first entree for To Html5",
+          "imgurl": "images/portfolio/boston.png",
+          "url": "https://hotelwebsiteproject.netlify.app"
+      },
+      {
+        "name":"Hotel Sleep",
+        "description":"My Group project i fell in love with",
+          "imgurl": "images/portfolio/hotelsleep.png",
+          "url": "https://hotelwebsiteproject.netlify.app/"
+      },
+      {
+        "name":"Hyper Machine",
+        "description":"My final Project using react js",
+          "imgurl": "images/portfolio/hyyper.png",
+          "url": "https://hypermachine.netlify.app/"
+      }
+    ],
+
+  }
+  
+  export default resumeData
+  
